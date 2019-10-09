35cfcd24addb7e3ab278d87b1776ac394dcf067c
moses-mugoya <mosesmugoya31@gmail.com>
2019-10-09 15:43:58 +0300

    auth styling

--- jumia/settings.py
+++ jumia/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'bootstrap4',
+    'crispy_forms',
     'import_export',
     'computed_property',
 ]
